880bb76a7c04abe1b5c6ead22050b148a2d28c9a
yukii <yuki27@hotmail.fr>
2017-07-19 12:06:15 +0200

    Add news tests in "src/test.c"

--- test/include/test.h
+++ test/include/test.h
@@ -1,9 +1,12 @@
 #include "my.h"
+
 #ifndef __TEST_H__
 #define __TEST_H__
 
-void assert_equal(int, int);
-void assert_not_equal(int, int);
+void assert_int_equal(int, int);
+void assert_int_not_equal(int, int);
+void assert_ptr_equal(void *, void *);
+void assert_ptr_not_equal(void *, void *);
 void assert_null(void *);
 void assert_list_equal(t_list *, t_list *, int (*)());
 
--- test/src/test.c
+++ test/src/test.c
@@ -3,35 +3,47 @@
 #include "my.h"
 #include "test.h"
 
-void assert_equal(int received, int expected)
+void assert_int_equal(int received, int expected)
 {
-	if (received == expected) {
+	if (received == expected)
 		printf("SUCCESS\n");
-	}
-	else {
+	else
 		printf("FAILURE value received: %d and expected: %d\n", received, expected);
-	}
 }
 
-void assert_not_equal(int received, int expected)
+void assert_int_not_equal(int received, int expected)
 {
-	if (received != expected) {
+	if (received != expected)
 		printf("SUCCESS\n");
-	}
-	else {
+	else
 		printf("FAILURE value received: %d and expected: %d\n", received, expected);
-	}
+}
+
+void assert_ptr_equal(void *received, void *expected)
+{
+	if (received == expected)
+		printf("SUCCESS\n");
+	else
+		printf("FAILURE value received: %p and expected: %p\n", received, expected);
+}
+
+void assert_ptr_not_equal(void *received, void *expected)
+{
+	if (received != expected)
+		printf("SUCCESS\n");
+	else
+		printf("FAILURE value received: %p and expected: %p\n", received, expected);
 }
 
 void assert_null(void *data)
 {
-	if (data == NULL) {
+	if (data == NULL)
 		printf("SUCCESS\n");
-	}
-	else {
+	else
 		printf("FAILURE value received: %p and expected: NULL\n", data);
-	}
 }
+
+
 
 void assert_list_equal(t_list *list, t_list *ref, int compare_fct())
 {
